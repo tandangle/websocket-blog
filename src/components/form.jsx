fd8afdbc6d9e3b869b17421f617390befae5ae23
Tandangle <tandangle76@gmail.com>
2022-03-10 02:04:21 -0500

    added websocket support, more styling

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Api } from '../api';
 
-const Form = () => {
+const Form = ({webSocket}) => {
     const [name, setName] = React.useState('');
     const [comment, setComment] = React.useState('');
+    const [error, setError] = React.useState('');
 
     const submitComment = e => {
         e.preventDefault();
+        setError('');
+        if (!name || !comment) {
+            setError('Please fill out all fields');
+            return;
+        }
         Api.post('/createComment', {
             name,
             message: comment
-        })
+        }).then(result => webSocket.send(JSON.stringify(result)))
     }
-
+    
     return (
         <form onSubmit={submitComment} className="comment-form">
             <label className="name-label">
@@ -23,6 +29,7 @@ const Form = () => {
                 <textarea className="comment-input" type="text" placeholder="Enter your message" value={comment} onChange={e => setComment(e.target.value)}/>
             </label>
             <button className="comment-form-button" type="submit">Comment</button>
+            {error && <div className="form-error">{error}</div>}
         </form>
     )
 }
